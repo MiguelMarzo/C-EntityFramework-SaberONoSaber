d8cbbef02c8b4f5648020545600467e868113a30
Miguel <miguel-marzo@hotmail.es>
2016-12-21 11:09:23 +0100

    Started Data Class

---
+++ CapaDatos/datos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class Datos
+    {
+        private SaberNoSaberEntities contexto;
+        
+        public Datos()
+        {
+            contexto = new SaberNoSaberEntities();  
+        }
+
+         
+    }
+}
